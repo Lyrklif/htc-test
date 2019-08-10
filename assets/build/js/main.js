c10ffb913afb3d7567bc532dcad7d39be5080f12
Lyrklif <kochurov121@gmail.com>
2019-08-10 08:25:31 +0400

    новые комментарии к функциям, обработка нажатия на email

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -13741,183 +13741,25 @@ function removeInterestForClick() {
         $(this).remove();
     });
 }; // Интересы  
-    // При клике на редактируемый текст (информация о пользователе)
-// появляется input, в котором можно изменить данные.
-// После этого новый текст изменяется и на странице,
-// а также сохранится в localStorage
-
-// $('.editText').on('click', function(event) {
-//     var editableElement = $(this); // Редактируемый элемент, на который нажали
-//     var oldText = editableElement.html(); // Старый текст элемента
-//     var editableElementID = editableElement.attr('id'); //ID редактируемого элемента
-
-//     // Создание input для ввода новой информации
-//     // var editInput = '<input class="inputEditUserInfo" type="text" name="newUserInfo" value="' + oldText + '">';
-
-
-
-//     var editInput = '<input class="inputEditUserInfo" type="text">';
-//     var inputEditUserInfo; // Созданный input
-
-
-//     switch (editableElementID) {
-//         // Редактирование имени пользователя
-//         case 'USER_NAME':
-//             editInput =
-//                 '<input class="inputEditUserInfo" type="text" name="newUserName" maxlength="20" minlength="2" value="' + oldText + '">';
-
-//             editableElement.after(editInput); // Добавить input после текста
-//             inputEditUserInfo = editableElement.next('.inputEditUserInfo'); // Найти созданный input
-//             break;
-
-//             // Редактирование телефона пользователя
-//         case 'USER_TEL':
-//             editInput =
-//                 '<input class="inputEditUserInfo" type="tel" name="newUserTel" maxlength="18" minlength="18" value="' + oldText + '">';
-//             editableElement.after(editInput); // Добавить input после текста
-//             inputEditUserInfo = editableElement.next('.inputEditUserInfo'); // Найти созданный input
-//             inputEditUserInfo.inputmask('+7 (999) 999-99-99');
-//             break;
-
-//             // Редактирование email пользователя
-//         case 'USER_EMAIL':
-//             editInput =
-//                 '<input class="inputEditUserInfo" type="email" name="newUserEmail" maxlength="20" minlength="6" value="' + oldText + '">';
-//             editableElement.after(editInput); // Добавить input после текста
-//             inputEditUserInfo = editableElement.next('.inputEditUserInfo'); // Найти созданный input
-//             inputEditUserInfo.inputmask('email');
-//             break;
-
-//         default:
-//             console.log("Новый редактируемый элемент. Для него нужно прописать правила в js");
-//             break;
-//     };
+    /**
+ * При нажатии на ИМЯ/ТЕЛЕФОН/EMAIL пользователя появляется input,
+ * в котором можно изменить эти данные
+ *
+ * Функции: 
+ * procedureCloseInput - Порядок действий при попытке закрыть input (нажатие Enter, потеря фокуса)
+ * isChangedText - Отличается ли введённый в input текст от старого?
+ * writeNewText - Записать новый текст в тег
+ * 
+ * isValidInputName - Проверка нового имени на валидность
+ * isValidInputTel - Проверка нового телефона на валидность
+ * isValidInputEmail - Проверка нового email на валидность
+ * 
+ * 
+ */
 
 
 
-
-//     inputEditUserInfo.focus(); // Поставить фокус на созданном input 
-
-//     // При нажатии на Enter, когда input в фокусе
-//     inputEditUserInfo.keydown(function(event) {
-//         if (event.which == 13) {
-//             writeNewText();
-//         }
-//     });
-
-//     // Когда элемент теряет фокус
-//     inputEditUserInfo.blur(function() {
-//         writeNewText();
-//     });
-
-//     // Отобразить новый текст на странице и закрыть input
-//     function writeNewText() {
-//         let newText = inputEditUserInfo.val(); // Текст, который пользователь изменил в input
-
-//         // Если текст отличается от того, который был изначально
-//         if (oldText != newText) {
-
-//             // Действия в зависимости от того, на какой элемент нажали
-//             switch (editableElementID) {
-//                 // Редактирование имени пользователя
-//                 case 'USER_NAME':
-//                     let isValidName = isValidInputName(newText);
-//                     if (isValidName) {
-//                         editableElement.text(newText); // Записать новый текст в тег
-//                         localStorage.setItem('userData.name', newText); // Обновить localStorage
-//                     } else {
-
-//                     };
-//                     break;
-
-//                     // Редактирование телефона пользователя
-//                 case 'USER_TEL':
-//                     let isValidTel = isValidInputTel(newText);
-//                     if (isValidTel) {
-//                         editableElement.text(newText); // Записать новый текст в тег
-//                         localStorage.setItem('userData.tel', newText); // Обновить localStorage
-//                     };
-//                     break;
-
-//                     // Редактирование email пользователя
-//                 case 'USER_EMAIL':
-//                     let isValidEmail = isValidInpuEmail(newText);
-//                     if (isValidEmail) {
-//                         editableElement.text(newText); // Записать новый текст в тег
-//                         localStorage.setItem('userData.email', newText); // Обновить localStorage
-//                     };
-//                     break;
-
-//                 default:
-//                     console.log("Новый редактируемый элемент. Для него нужно прописать правила в js");
-//                     break;
-//             };
-
-//         };
-
-//         inputEditUserInfo.remove(); // удалить input
-
-//     };
-
-
-
-// // Проверка нового номера телефона на валидность
-// function isValidInputTel(a) {
-//     let newText = a,
-//         isValid;
-
-//     let newTextLenth = newText.length;
-
-//     if (newTextLenth == 18) {
-//         isValid = true;
-//     }
-
-//     console.log(newTextLenth, isValid);
-
-//     return isValid;
-// };
-
-// // Проверка нового email на валидность
-// function isValidInpuEmail(a) {
-//     let newText = a,
-//         isValid;
-//     newTextLenth = newText.length;
-
-
-//     var regex = /^[a-z0-9_-]+@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i;
-//     regexValue = regex.test(newText);
-
-//     if (newTextLenth > 6 && newTextLenth <= 20 && regexValue) {
-//         isValid = true;
-//     }
-
-
-//     console.log(newTextLenth, isValid);
-
-//     return isValid;
-// };
-
-
-
-
-
-
-
-
-
-
-// ********************************************************
-// ********************************************************
-// ********************************************************
-// ********************************************************
-// ********************************************************
-// ********************************************************
-// ********************************************************
-
-
-
-
-
+// Изменение ИМЕНИ пользователя
 $('#USER_NAME').on('click', function(event) {
     let name = 'name';
     var editableElement = $(this); // Редактируемый элемент, на который нажали
@@ -13947,6 +13789,7 @@ $('#USER_NAME').on('click', function(event) {
 });
 
 
+// Изменение ТЕЛЕФОНА пользователя
 $('#USER_TEL').on('click', function(event) {
     let tel = 'tel';
     var editableElement = $(this); // Редактируемый элемент, на который нажали
@@ -13977,7 +13820,65 @@ $('#USER_TEL').on('click', function(event) {
 });
 
 
-// Порядок действий при попытке закрыть input (нажатие Enter, потеря фокуса)
+// Изменение EMAIL пользователя
+$('#USER_EMAIL').on('click', function(event) {
+    let email = 'email';
+    var editableElement = $(this); // Редактируемый элемент, на который нажали
+    var oldText = editableElement.html(); // Старый текст элемента
+
+    // input для ввода новой информации
+    editInput =
+        '<input class="inputEditUserInfo" type="email" name="newUserEmail" minlength="6" maxlength="20" value="' + oldText + '">';
+
+
+    editableElement.after(editInput); // Добавить input после текста
+    inputEditUserInfo = editableElement.next('.inputEditUserInfo'); // Найти созданный input
+    // inputEditUserInfo.inputmask('email');
+    inputEditUserInfo.inputmask({
+        mask: "email",
+        greedy: false,
+        onBeforePaste: function(pastedValue, opts) {
+            pastedValue = pastedValue.toLowerCase();
+            return pastedValue.replace("mailto:", "");
+        },
+        definitions: {
+            '*': {
+                validator: "[/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/]",
+                casing: "lower"
+            }
+        }
+    });
+    // inputEditUserInfo.attr({
+    //     data - inputmask - regex: '[/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/]',
+    //     title: 'Введите адрес электронной почты в формате: vitalya@gora.ru'
+    // });
+
+    inputEditUserInfo.focus(); // Поставить фокус на созданном input 
+
+
+    // При нажатии на Enter, когда input в фокусе
+    inputEditUserInfo.keydown(function(event) {
+        if (event.which == 13) {
+            procedureCloseInput(editableElement, inputEditUserInfo, oldText, isValidInputEmail, email);
+        }
+    });
+
+    // Когда элемент теряет фокус
+    inputEditUserInfo.blur(function() {
+        procedureCloseInput(editableElement, inputEditUserInfo, oldText, isValidInputEmail, email);
+    });
+});
+
+
+
+/**
+ * Порядок действий при попытке закрыть input (нажатие Enter, потеря фокуса)
+ * @param  {[type]} editableElement      [Редактируемый элемент, на который нажали]
+ * @param  {[type]} inputEditUserInfo    [Созданный input]
+ * @param  {[type]} oldText              [Старый текст элемента]
+ * @param  {[type]} nameFunctionIsValid  [Проверка нового ИМЕНИ на валидность]
+ * @param  {[type]} localStorageItemName [Название элемента localStorage, в который нужно записать новое значение]
+ */
 function procedureCloseInput(editableElement, inputEditUserInfo, oldText, nameFunctionIsValid, localStorageItemName) {
     let newText = inputEditUserInfo.val(); // Текст, который пользователь ввёл в input 
     let changedText = isChangedText(oldText, newText); // Отличается ли текст от старого? 
@@ -13994,43 +13895,85 @@ function procedureCloseInput(editableElement, inputEditUserInfo, oldText, nameFu
     };
 };
 
-// Проверка нового имени на валидность
+
+/**
+ * Проверка нового ИМЕНИ на валидность
+ * @param  {[type]}  newText [Текст, который пользователь ввёл в input]
+ * @return {Boolean}         [true, если новый текст валиден]
+ */
 function isValidInputName(newText) {
     let isValid = false;
 
-    let newTextLenth = newText.length;
+    let newTextLength = newText.length;
 
-    if (newTextLenth >= 2 && newTextLenth <= 20) {
+    if (newTextLength >= 2 && newTextLength <= 20) {
         isValid = true;
     }
 
-    console.log(newTextLenth, isValid);
+    console.log('Введённое имя:', newText);
+    console.log('Количество символов:', newTextLength);
+    console.log('Валидность введённых данных:', isValid);
 
     return isValid;
 };
 
-// Проверка нового номера телефона на валидность
+
+/**
+ * Проверка нового ТЕЛЕФОНА на валидность
+ * @param  {[type]}  newText [Текст, который пользователь ввёл в input]
+ * @return {Boolean}         [true, если новый текст валиден]
+ */
 function isValidInputTel(newText) {
     let isValid = false;
-    let newTextLenth = newText.length;
+    let newTextLength = newText.length;
 
-    let isValidFormat = /^\+[7]{1}\ \([\d]{3}\)\ [\d]{3}-[\d]{2}-[\d]{2}$/.test(str);
+    let isValidFormat = /^\+[7]{1}\ \([\d]{3}\)\ [\d]{3}-[\d]{2}-[\d]{2}$/.test(newText);
     telephoneCheck(newText);
-    if (newTextLenth == 18 && isValidFormat) {
+    if (newTextLength == 18 && isValidFormat) {
         isValid = true;
     }
 
+    console.log('Введённый телефон:', newText);
     console.log('Верен ли формат телефона:', isValidFormat);
-    console.log('Количество символов:', newTextLenth);
-    console.log('Валидность введённых данных:' isValid);
+    console.log('Количество символов:', newTextLength);
+    console.log('Валидность введённых данных:', isValid);
 
     return isValid;
 };
 
 
-// Отличается ли текст от старого?
+/**
+ * Проверка нового EMAIL на валидность
+ * @param  {[type]}  newText [Текст, который пользователь ввёл в input]
+ * @return {Boolean}         [true, если новый текст валиден]
+ */
+function isValidInputEmail(newText) {
+    let isValid = false;
+    newTextLength = newText.length;
+
+    let isValidFormat = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/.test(newText);
+
+    if (newTextLength >= 6 && newTextLength <= 20 && isValidFormat) {
+        isValid = true;
+    }
+
+    console.log('Введённый email:', newText);
+    console.log('Верен ли формат email:', isValidFormat);
+    console.log('Количество символов:', newTextLength);
+    console.log('Валидность введённых данных:', isValid);
+
+    return isValid;
+};
+
+
+/**
+ * Отличается ли введённый в input текст от старого?
+ * @param  {[type]}  oldText [Старый текст]
+ * @param  {[type]}  newText [Текст, который пользователь ввёл в input]
+ * @return {Boolean}         [true, если введённый текст отличается от старого]
+ */
 function isChangedText(oldText, newText) {
-    let isNew;
+    let isNew = false;
 
     if (oldText != newText) {
         isNew = true;
@@ -14040,7 +13983,12 @@ function isChangedText(oldText, newText) {
 };
 
 
-// Записать новый текст в тег
+/**
+ * Записать новый текст в тег
+ * @param  {[type]} editableElement      [Редактируемый элемент, на который нажали]
+ * @param  {[type]} newText              [Текст, который пользователь ввёл в input]
+ * @param  {[type]} localStorageItemName [Название элемента localStorage, в который нужно записать новое значение]
+ */
 function writeNewText(editableElement, newText, localStorageItemName) {
     editableElement.text(newText); // Записать новый текст в тег
     localStorage.setItem('userData.' + localStorageItemName, newText); // Обновить localStorage
